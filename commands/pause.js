5678d6f87d97886e16cb5b8932f8ae621e39cba6
PGamerX <websitedesigner123456@gmail.com>
2020-12-30 22:11:33 +0530

    Create pause.js

---
+++ commands/pause.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "pause",
+    aliases: ["pause", "hold"],
+    run: async (client, message, args) => {
+        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
+        let queuethere = distube.getQueue(message);
+         if (!queuethere) return message.channel.send(`${client.emotes.error} There is nothing in the queue right now!`)
+         client.distube.pause(message)
+            message.channel.send(`Paused the song for you :)`)
+        } catch (e) {
+            message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
+        }
+    }
+}
