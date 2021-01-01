f671a421b24c187275b7712c75f13d8465e468b2
PGamerX <websitedesigner123456@gmail.com>
2021-01-01 17:28:53 +0530

    Update commands/pause.js
    Co-authored-by: Skick <skick1234@gmail.com>

--- commands/pause.js
+++ commands/pause.js
@@ -3,9 +3,13 @@ module.exports = {
     aliases: ["pause", "hold"],
     run: async (client, message, args) => {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
-        let queuethere = client.distube.getQueue(message);
-         if (!queuethere) return message.channel.send(`${client.emotes.error} There is nothing in the queue right now!`)
-         client.distube.pause(message)
+        let queue = client.distube.getQueue(message)
+        if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        if (queue.pause) {
+            client.distube.resume(message)
+            return message.channel.send("Resumed the song for you :)")
+        }
+        client.distube.pause(message)
             message.channel.send(`Paused the song for you :)`)
         } catch (e) {
             message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
