8d819d041e82c3166a1dd8740ced5ecefb494146
PGamerX <websitedesigner123456@gmail.com>
2021-01-01 17:29:51 +0530

    Update commands/resume.js
    Co-authored-by: Skick <skick1234@gmail.com>

--- commands/resume.js
+++ commands/resume.js
@@ -3,9 +3,9 @@ module.exports = {
     aliases: ["resume", "unpause"],
     run: async (client, message, args) => {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
-        let queuethere = client.distube.getQueue(message);
-         if (!queuethere) return message.channel.send(`${client.emotes.error} There is nothing in the queue right now!`)
-         client.distube.resume(message)
+        let queue = client.distube.getQueue(message)
+        if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        client.distube.resume(message)
             message.channel.send(`Resumed the song for you :)`)
         } catch (e) {
             message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
