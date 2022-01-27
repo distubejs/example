26bcacbda4549521767135f959ea2c5b2e03be4d
Skick <skick1234@gmail.com>
2022-01-27 21:36:23 +0700

    Remove deprecation stuff

--- commands/playskip.js
+++ commands/playskip.js
@@ -5,6 +5,11 @@ module.exports = {
     run: async (client, message, args) => {
         const string = args.join(" ")
         if (!string) return message.channel.send(`${client.emotes.error} | Please enter a song url or query to search.`)
-        client.distube.play(message, string, { skip: true })
+        client.distube.play(message.member.voice.channel, string, {
+          member: message.member,
+          textChannel: message.channel,
+          message,
+          skip: true
+      })
     }
 }
