21d6c8a1e44a169e5b5463c53af5820e679c3e24
Mahmoud <whixat@gmail.com>
2022-07-15 07:51:11 +0200

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -5,7 +5,11 @@ module.exports = {
   run: async (client, message) => {
     const queue = client.distube.getQueue(message)
     if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
-    queue.resume()
-    message.channel.send('Resumed the song for you :)')
+      if (queue.paused) {
+        queue.resume()
+        message.channel.send('Resumed the song for you :)')
+      } else {
+        message.channel.send('The queue is not paused!')
+      }
   }
 }
