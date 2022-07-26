e481253717b9b7b841f968b2bd95194cdb9025ee
Mahmoud <whixat@gmail.com>
2022-07-26 02:48:04 +0200

    Create skipto.js

---
+++ commands/skipto.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'skipto',
+  inVoiceChannel: true,
+  run: async (client, message, args) => {
+    const queue = client.distube.getQueue(message)
+    if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+    if (!args[0]) {
+      return message.channel.send(`${client.emotes.error} | Please provide time (in seconds) to go rewind!`)
+    }
+    const num = Number(args[0])
+    if (isNaN(num)) return message.channel.send(`${client.emotes.error} | Please enter a valid number!`)
+    await client.distube.jump(message, num).then((queue) => {
+      msg.edit({ content: `Skipped to: ${num}` });
+    })
+  }
+}
