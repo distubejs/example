d9573b84851bb0894345ed760c81ef4864eb6c8c
Aboda7m <50844122+Aboda7m@users.noreply.github.com>
2022-10-21 23:58:54 +0300

    Update queue.js
    fixed error when the queue message exceeds discord's 4000 character limit

--- commands/queue.js
+++ commands/queue.js
@@ -7,6 +7,18 @@ module.exports = {
     const q = queue.songs
       .map((song, i) => `${i === 0 ? 'Playing:' : `${i}.`} ${song.name} - \`${song.formattedDuration}\``)
       .join('\n')
-    message.channel.send(`${client.emotes.queue} | **Server Queue**\n${q}`)
+    //message.channel.send(`${client.emotes.queue} | **Server Queue**\n${q}`)
+	var str = `${client.emotes.queue} | **Server Queue**\n${q}`;
+	var partsArr = str.match(/[\s\S]{1,1900}/g) || [];
+	if (partsArr.length > 1) {
+		partsArr.forEach((partsArr, i) =>    message.channel.send(`${partsArr} \nPart ${i + 1} / ${partsArr.length}`));
+	}
+	else {
+	message.channel.send(`${client.emotes.queue} | **Server Queue**\n${q}`);
+	}
+			
+		
+
+	
   }
 }
